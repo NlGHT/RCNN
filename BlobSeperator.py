6e4128ed1b36c81e30e529401cfa48c8a4e3ffa5
NlGHT <tnight17@student.aau.dk>
2020-11-11 12:43:05 +0100

    Put the detection in function

--- BlobSeperator.py
+++ BlobSeperator.py
@@ -2,51 +2,64 @@
 import cv2
 import numpy as np
 
-print("Hello")
+def detectImage(image):
+    # Read image
+    im = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
 
-# Read image
-im = cv2.imread("000000000471-3.png", cv2.IMREAD_GRAYSCALE)
+    # Setup SimpleBlobDetector parameters.
+    params = cv2.SimpleBlobDetector_Params()
 
-# Setup SimpleBlobDetector parameters.
-params = cv2.SimpleBlobDetector_Params()
-
-# Change thresholds
-params.minThreshold = 10
-params.maxThreshold = 200
+    # Change thresholds
+    params.minThreshold = 10
+    params.maxThreshold = 250
 
 
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 15
+    # Filter by Area.
+    params.filterByArea = True
+    params.minArea = 15
+    params.maxArea = 10000
 
-# Filter by Circularity
-params.filterByCircularity = False
-params.minCircularity = 0.1
+    # Filter by Circularity
+    params.filterByCircularity = False
+    params.minCircularity = 0.1
 
-# Filter by Convexity
-params.filterByConvexity = False
-params.minConvexity = 0.87
+    # Filter by Convexity
+    params.filterByConvexity = False
+    params.minConvexity = 0.87
 
-# Filter by Inertia
-params.filterByInertia = False
-params.minInertiaRatio = 0.01
+    # Filter by Inertia
+    params.filterByInertia = False
+    params.minInertiaRatio = 0.01
 
-# Create a detector with the parameters
-detector = cv2.SimpleBlobDetector(params)
+    params.blobColor = 255
 
-print("Hello")
+    # Create a detector with the parameters
+    detector = cv2.SimpleBlobDetector_create(params)
 
-# Detect blobs.
-keypoints = detector.detect(im)
+    print("Hello")
 
-print("Hell")
+    # Detect blobs.
+    keypoints = detector.detect(im)
 
-# Draw detected blobs as red circles.
-# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-# the size of the circle corresponds to the size of blob
+    print("Hell")
 
-im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # Draw detected blobs as red circles.
+    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
+    # the size of the circle corresponds to the size of blob
 
-# Show blobs
-cv2.imshow("Keypoints", im_with_keypoints)
-cv2.waitKey(0)
+    im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Show blobs
+    cv2.imshow("Keypoints", im_with_keypoints)
+    cv2.waitKey(0)
+
+
+# Load all the files with -3
+import os
+import sys
+SOBA_DIR = "./SOBA/SOBA/"
+for dirname in os.listdir(SOBA_DIR):
+    for image in os.listdir(SOBA_DIR + dirname):
+        if image[-6:] == "-3.png":
+            detectImage(image)
+            break
